8e05a76802d18b5fc2a09657ed8cfc058bd61d34
Vitaly <vitalyasatka@gmail.com>
2023-05-06 18:09:28 +0500

    feat: add solutions

---
+++ 01-read-file/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const fs = require('fs');
+const { stdout } = process;
+
+const readableStream = fs.createReadStream(
+  path.join(__dirname, 'text.txt'),
+  'utf-8'
+);
+
+readableStream.on('data', (data) => {
+  stdout.write(data);
+});
+readableStream.on('error', (error) => {
+  stdout.write(error.message);
+});
