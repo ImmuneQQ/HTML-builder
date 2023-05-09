cb50ec5bfffc161bf6f733ead10b84a7b614453b
Vitaly <vitalyasatka@gmail.com>
2023-05-10 00:07:30 +0500

    feat: add delete old files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,20 +1,31 @@
 const path = require('node:path');
-const { readdir, mkdir, copyFile } = require('node:fs/promises');
+const { readdir, mkdir, copyFile, unlink } = require('node:fs/promises');
 
 async function copyDir(pathToDir, dir) {
   await mkdir(path.join(pathToDir, `${dir}-copy`), { recursive: true });
+
+  const oldFiles = await readdir(path.join(pathToDir, `${dir}-copy`), {
+    withFileTypes: true,
+  });
+  for (let file of oldFiles) {
+    if (file.isFile()) {
+      await unlink(
+        path.join(pathToDir, `${dir}-copy`, file.name)
+      );
+    }
+  }
+
   const files = await readdir(path.join(pathToDir, dir), {
     withFileTypes: true,
   });
-
-  files.forEach((file) => {
+  for (let file of files) {
     if (file.isFile()) {
-      copyFile(
+      await copyFile(
         path.join(pathToDir, dir, file.name),
         path.join(pathToDir, `${dir}-copy`, file.name)
       );
     }
-  });
+  }
 }
 
 copyDir(__dirname, 'files');
